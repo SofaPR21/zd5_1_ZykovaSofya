002a9d544c02914f8b867f7f07ff3c10dafa58c1
Sofya <he.october1510@gmail.com>
2025-06-14 21:50:27 +0500

    first commit

---
+++ zd4_Zykova_Sofya/zd4_Zykova_Sofya/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zd4_Zykova_Sofya
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart(){}
+
+        protected override void OnSleep(){}
+
+        protected override void OnResume(){}
+    }
+}
